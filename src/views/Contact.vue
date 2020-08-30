38e6edf2fc9678e2270d99588dbbd81db6f6f83d
anujpatidar3 <anujpatidar3@gmail.com>
2020-08-30 14:34:05 +0530

    facebbok login test

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,16 +1,63 @@
 <template>
   <div class="Contact">
     <Contactcontent></Contactcontent>
+    <div style="margin-top:3rem">
+      <facebook-login class="button"
+      appId="2867441940158712"
+      @login="onLogin"
+      @logout="onLogout"
+      @sdk-loaded="sdkLoaded">
+    </facebook-login>
+    </div>
   </div>
 </template>
 
 <script>
-
+import facebookLogin from 'facebook-login-vuejs';
 import Contactcontent from '../components/contact/contactcontent'
+
 export default {
   name: "Contact",
   components: {
-   Contactcontent
+   Contactcontent,
+   facebookLogin
+  },
+  
+  data(){
+    return{
+      isConnected: false,
+      name: '',
+      email: '',
+      personalID: '',
+      FB: undefined
+    }
+  },
+  methods: { 
+  getUserData() {
+      this.FB.api('/me', 'GET', { fields: 'id,name,email' },
+        userInformation => {
+          console.warn("data api",userInformation)
+          this.personalID = userInformation.id;
+          this.email = userInformation.email;
+          this.name = userInformation.name;
+        }
+      )
+    },
+    sdkLoaded(payload) {
+      console.log("sdk loaded")
+      this.isConnected = payload.isConnected
+      this.FB = payload.FB
+      if (this.isConnected) this.getUserData()
+    },
+    onLogin() {
+      console.log("loogin called")
+      this.isConnected = true
+      this.getUserData()
+    },
+    onLogout() {
+      console.log("logout called")
+      this.isConnected = false;
+    }
   }
-};
+}
 </script>
